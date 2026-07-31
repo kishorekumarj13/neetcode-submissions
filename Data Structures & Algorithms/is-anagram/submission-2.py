d25fe5d14cd0d8598b0dd74b6ffa385e89c1edd2
Kishore Kumar J <99961606+kishorekumarj13@users.noreply.github.com>
2026-07-31 23:52:22 +0530

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return sorted(s) == sorted(t)
+        if len(s) != len(t):
+            return False
+        else:
+            return self._get_alphabet_count(s) == self._get_alphabet_count(t)
+    def _get_alphabet_count(self, word: str) -> dict:
+        letters = list(set(sorted(word)))
+        alphabet_count = {}
+        for i in range(len(letters)):
+            alphabet_count[letters[i]] = word.count(letters[i])
+        return alphabet_count
+
+        
